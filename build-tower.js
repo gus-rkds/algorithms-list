a63b0b77465dac13de0622bb010d76bc2d2e5385
Gustavo <gusalv2001@gmail.com>
2022-05-23 18:51:12 -0400

    added build tower and higher scoring

---
+++ build-tower.js
@@ -0,0 +1,23 @@
+function towerBuilder(nFloors) {
+  let tower = [];
+  for (let i = 0; i < nFloors; i++) {
+    tower.push(
+      " ".repeat(nFloors - i - 1) +
+        "*".repeat(i * 2 + 1) +
+        " ".repeat(nFloors - i - 1)
+    );
+  }
+  return tower;
+}
+
+//   And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ",
+//   "    ***    ",
+//   "   *****   ",
+//   "  *******  ",
+//   " ********* ",
+//   "***********"
+// ]
+// Go challenge Build Tower Advanced once you have finished this :)
